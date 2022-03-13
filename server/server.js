0b44f90cb1583ec297e34b821efc66cd7a1a49c3
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-03-13 21:50:57 +0300

    added dashboard compponent

--- server/server.js
+++ server/server.js
@@ -3,4 +3,22 @@ const spotifyWebApi = require('spotify-web-api');
 
 const app = express();
 
-app.post()
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId:'b7d4c717a999468487cc8a213f69c46a',
+        clientSecret: '11c3a850d1ed4dfaad77b103bd7f206f'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    })
+    .catch(() => {
+        res.sendStatus(400)
+    })
+})
